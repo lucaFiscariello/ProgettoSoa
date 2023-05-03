51bf2d1294364e7368a50896cbf06f0e56ff4ce7
lucaFiscariello <lucafiscariello@gmail.com>
2023-05-03 19:09:36 +0200

    Aggiunta commenti

--- message-service/core-RCU/block_read_write.c
+++ message-service/core-RCU/block_read_write.c
@@ -1,10 +1,26 @@
+/**
+ * In questo file sono implementate le funzioni che permettono di leggere e scrivere un blocco su un device.
+ * Il livello di astrazione è tale per cui le funzioni si aspettano di scrivere e leggere blocchi "pronti". Non è responsabilità 
+ * di queste funzioni creare il blocco e popolarlo con dati e metadati. Si assume che il blocco sia stato già arricchito dalle informazioni
+ * necessarie. In particolare un blocco del device è modellato tramite la struttura "block" che prevede una serie di campi 
+ * per mantenere metadati e dati utili.
+*/
+
 #include <linux/kernel.h>
 #include <linux/buffer_head.h>
 #include <linux/blkdev.h>
 #include <linux/string.h>
 #include "lib/include/block_read_write.h"
 
-
+/**
+ * Tale funzione è usata nella scrittura di un blocco. Il suo compito è quello di trovare un blocco libero in cui poter scrivere nuove 
+ * informazioni. Come prima cosa il metodo  cerca il prossimo blocco libero all'interno del campo "nextFreeBlock" del metablocco. Se tale
+ * campo contiene un valore non valido allora si cercherà un nuovo blocco nella lista dei blocchi precedentemente invalidati e mantenuta 
+ * sempre nel metablocco. 
+ * La lista contiene SOLO blocchi non validi per cui è sufficiente prelevare l'elemento in testa alla lista. Questo rende la ricerca del prossimo
+ * blocco in cui scrivere costante e non lineare.
+ * Se non sono presenti blocchi liberi è restituito il valore BLOCK_ERROR.
+*/
 int get_next_free_block(){
 
     int nextFreeBlock = BLOCK_ERROR;
@@ -14,13 +30,17 @@ int get_next_free_block(){
     meta_block_rcu= read_ram_metablk();
     block_device = get_block_device_AfterMount();
 
+    // Controllo il campo nextFreeBlock del metablocco
     if(meta_block_rcu->nextFreeBlock+1 < meta_block_rcu->blocksNumber){
         meta_block_rcu->nextFreeBlock++;
         nextFreeBlock = meta_block_rcu->nextFreeBlock;
     }
+
+    // Prelevo il primo elemento della lista dei blocchi non validi
     else{
         nextFreeBlock = meta_block_rcu->headInvalidBlock->block;
 
+        //Verifico se il primo elemento della lista è valido
         if(meta_block_rcu->headInvalidBlock->next != NULL)
             meta_block_rcu->headInvalidBlock = meta_block_rcu->headInvalidBlock->next;
         else
@@ -32,6 +52,13 @@ int get_next_free_block(){
 }
 
 
+/**
+ * Tale funzione si occupa di leggere un blocco dal device all'offset "block_to_read" e memorizzarlo nella struttura "block".
+ * La lettura sfrutta API Linux per implementare rcu. In particolare:
+ *  - rcu_read_lock : è un lock per i soli scrittori. I lettori possono concorrere senza problemi
+ *  - rcu_dereference : è una macro che implementa un'assegnazione atomica attraverso ottimizzazioni al compilatore per l'accesso in memoria
+ *  - rcu_read_unlock : libera eventuali scrittori in attesa.
+*/
 int read( int block_to_read,struct block* block){
     struct meta_block_rcu *meta_block_rcu;
     struct buffer_head *bh = NULL;
@@ -63,6 +90,15 @@ int read( int block_to_read,struct block* block){
 
 }
 
+/**
+ * Questa funzione legge tutti i blocchi validi mantenuti nel device. Tutti i blocchi validi sono collegati tra di loro 
+ * attraverso una serie di informazioni mantenute dai metadati del blocco stesso. In particolare ogni blocco nei propri metadati mantiene
+ * gli ID dei blocchi scritti temporalmente prima e dopo di lui. In questo modo è implementata a tutti gli effetti una lista doppiamente collegata.
+ * La resposabilità della funzione è quindi quella di individuare l'offset del primo blocco  valido scritto e scorrere la lista 
+ * per leggere tutti i blocchi validi. L'ID del primo blocco valido è mantenuto nel meta blocco.
+ * La lettura è implementata utilizzando le API Linux rcu, pertanto un lettore leggerà tutti i blocchi ancora validi alla chiamata di 
+ * "rcu_read_lock" anche se è presente uno scrittore concorrente.
+*/
 int read_all_block(char* data){
 
     struct buffer_head *bh = NULL;
@@ -78,7 +114,8 @@ int read_all_block(char* data){
     temp_block = kmalloc(DIM_BLOCK, GFP_KERNEL);
 
     rcu_read_lock();
-
+    
+    // Leggo primo blocco valido
     bh = (struct buffer_head *)sb_bread(block_device->bd_super, temp_block_to_read);
     temp = (void*) rcu_dereference(bh->b_data);
 
@@ -88,6 +125,7 @@ int read_all_block(char* data){
         concat_data(data,temp_block);
         brelse(bh);
 
+        // Scorro la lista dei blocchi validi
         while (temp_block->next_block != BLOCK_ERROR){
 
             temp_block_to_read = temp_block->next_block;
@@ -97,6 +135,8 @@ int read_all_block(char* data){
 
 
             if (temp != NULL){ 
+
+                // Concateno tutti i dati letti in un unico buffer
                 memcpy( temp_block,temp, DIM_BLOCK);
                 concat_data(data,temp_block);
             }
@@ -115,6 +155,14 @@ int read_all_block(char* data){
 
 }
 
+/**
+ * Questa funzione implementa la scrittura di un blocco nel device. Sfrutta API Linux rcu.
+ * In particolare le api Linux utilizzate sono:
+ *  - rcu_assign_pointer :  è una macro che implementa un'assegnazione atomica attraverso ottimizzazioni al compilatore per l'accesso in memoria
+ *  - synchronize_rcu: attende la fine del greece period
+ * Terminata l'attesa del greece period è possibile eliminare il buffer precedente in quanto nessun lettore lo sta più utilizzando.
+ * 
+ */
 int write(int block_to_write,struct block* block){
 
     struct block_device *block_device = get_block_device_AfterMount();
@@ -128,6 +176,7 @@ int write(int block_to_write,struct block* block){
       
     if (bh->b_data != NULL){
 
+        // Memorizzio il riferimento ai dati in una variabile temporanea. In questo modo alla fine del greece period è possibile deallocarla
         temp = bh->b_data;
         rcu_assign_pointer(bh->b_data,(void *) block);
         synchronize_rcu();
--- message-service/core-RCU/meta_block.c
+++ message-service/core-RCU/meta_block.c
@@ -1,3 +1,13 @@
+/**
+ * Questo file contiene l'implementazioni di funzioni che permettono di accedere al "meta blocco". 
+ * In questo progetto il meta blocco è un blocco di dati memorizzato nel device è che mantiene una serie di 
+ * informazioni utili per getire la scrittura e la lettura concorrente di altri blocchi nel device. 
+ * In particolare le funzioni implementate sono quelle di: 
+ *  - inizializzazione del metablocco
+ *  - flush del meta blocco dalla ram al device
+ *  - lettura del meta blocco dal device alla ram
+*/
+
 #include <linux/kernel.h>
 #include <linux/buffer_head.h>
 #include <linux/blkdev.h>
@@ -12,18 +22,13 @@ static int blocks_number = 0x0;
 
 module_param(blocks_number, int, 0660);
 
-void set_block_device_onMount(char* devname){
-    block_device= blkdev_get_by_path(devname, FMODE_READ|FMODE_WRITE, NULL);
-}
 
-void set_block_device_onUmount(){
-    block_device= NULL;
-}
-
-struct block_device * get_block_device_AfterMount(void){
-    return block_device;
-}
-
+/**
+ *  L'inizializzazione del meta blocco consiste nel compilare i campi della struttura "meta_block_rcu" con valori di default 
+ *  con conseguente scrittura della struttura all'interno di un blocco del device.
+ *  Tale funzione deve essere invocata nel momento in cui un device è montato per la prima volta in assoluto, in modo da 
+ *  formattare correttamente la struttura "meta_block_rcu" e garantire il corretto funzionamento delle API che dipendono da tale blocco.
+*/
 void inizialize_meta_block(){
     struct buffer_head *bh = NULL;
     struct invalid_block* head;
@@ -51,6 +56,11 @@ void inizialize_meta_block(){
     brelse(bh);
 }
 
+/**
+ * Il meta blocco è salvato in un blocco del device, tuttavia per motivi di prestazione è mantenuta una copia del blocco anche in ram.
+ * Ne consegue che se il meta blocco è soggetto a modifiche è necessario effettuare il flush di tale modifiche anche nel device. 
+ * Tale funzione ha proprio questa responsabilità.
+*/
 void flush_device_metablk(){
 
     struct buffer_head *bh = NULL;
@@ -60,7 +70,12 @@ void flush_device_metablk(){
     bh = (struct buffer_head *)sb_bread(block_device->bd_super, POS_META_BLOCK);
       
     if (bh->b_data != NULL){
-
+        
+        /**
+         * Memorizzo nel meta blocco anche gli id dei blocchi invalidati. In ram tutti gli id dei blocchi sono mantenuti 
+         * in una linked list. Nel meta blocco sono invece memorizzati in un array. Il ciclo while effetua la conversione
+         * da linked list a array.
+        */
         while(head->next != NULL && head->block != BLOCK_ERROR ){
             
             meta_block_rcu->invalidBlocks[pos++] = head->block;
@@ -74,13 +89,11 @@ void flush_device_metablk(){
     brelse(bh);
 }
 
-struct meta_block_rcu* read_ram_metablk(){
-    if(meta_block_rcu==NULL)
-        return read_device_metablk();
-
-    return meta_block_rcu;
-}
-
+/**
+ * Dualmente al flush del meta blocco nel device , questa funzione permette di leggere il metablocco dal device e salvare 
+ * una copia in ram. I campi del meta blocco sono accessibili sia in scrittura che in lettura. Eventuali modifiche devono essere opportunamente
+ * riportate sul device attraverso la funzione "flush_device_metablk".
+*/
 struct meta_block_rcu* read_device_metablk(){
 
     struct buffer_head *bh = NULL;
@@ -90,6 +103,11 @@ struct meta_block_rcu* read_device_metablk(){
       
     if (bh->b_data != NULL){ 
 
+       /**
+         * Nel meta blocco sono memorizzati anche gli id dei blocchi invalidati. In ram tutti gli id dei blocchi sono mantenuti 
+         * in una linked list. Nel meta blocco sono invece memorizzati in un array. Il ciclo for effetua la conversione
+         * da array a  linked list .
+        */
         for(int i=0 ; i<meta_block_rcu->invalidBlocksNumber; i++){
             new_invalid_block = kmalloc(sizeof(struct invalid_block),GFP_KERNEL);
             new_invalid_block->block = meta_block_rcu->invalidBlocks[i];
@@ -102,6 +120,29 @@ struct meta_block_rcu* read_device_metablk(){
 
     brelse(bh);
     return meta_block_rcu;
+}
+
+/**
+ * Tale funzione permette di implementare una cache in ram. Quando un thread ha bisogno di leggere il meta blocco 
+ * se è già presente in ram non c'è necessita di leggerlo nuovamente dal device.
+*/
+struct meta_block_rcu* read_ram_metablk(){
+    if(meta_block_rcu==NULL)
+        return read_device_metablk();
+
+    return meta_block_rcu;
+}
+
+void set_block_device_onMount(char* devname){
+    block_device= blkdev_get_by_path(devname, FMODE_READ|FMODE_WRITE, NULL);
+}
+
+void set_block_device_onUmount(){
+    block_device= NULL;
+}
+
+struct block_device * get_block_device_AfterMount(void){
+    return block_device;
 }
 
 
