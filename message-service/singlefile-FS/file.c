864c9822f9d13aa363dbef8d8adcd5f19bf74495
lucaFiscariello <lucafiscariello@gmail.com>
2023-06-14 11:52:58 +0200

    risoluzione problemi compilazione

--- message-service/singlefile-FS/file.c
+++ message-service/singlefile-FS/file.c
@@ -8,6 +8,7 @@
 #include <linux/types.h>
 #include <linux/slab.h>
 #include <linux/string.h>
+#include <linux/version.h>	
 
 #include "lib/include/singlefilefs.h"
 #include "../core-RCU/lib/include/rcu.h"
@@ -129,7 +130,13 @@ struct dentry *onefilefs_lookup(struct inode *parent_inode, struct dentry *child
 
 
 	//this work is done if the inode was not already cached
-    inode_init_owner(&init_user_ns, the_inode, NULL, S_IFREG);// set the root user as owned of the FS root
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0)
+	    inode_init_owner(&init_user_ns,the_inode, NULL, S_IFREG);
+#else
+        inode_init_owner(the_inode, NULL, S_IFREG);
+#endif
+
+
 	the_inode->i_mode = S_IFREG | S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IXUSR | S_IXGRP | S_IXOTH;
     the_inode->i_fop = &onefilefs_file_operations;
 	the_inode->i_op = &onefilefs_inode_ops;
